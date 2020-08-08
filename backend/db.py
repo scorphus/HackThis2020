d313c0a8f03b2623fdce4046c1afe72b8aabbc5a
Noelle Crawford <38481226+noelle-crawfish@users.noreply.github.com>
2020-08-08 05:32:06 -0700

    Delete db.py

--- backend/db.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from flask_pymongo import pymongo
-
-client = pymongo.MongoClient("mongodb+srv://user:thepassword@hackthis2020.txqxe.mongodb.net/<dbname>?retryWrites=true&w=majority")
-db = client.get_database('hackthis2020')
-user_collection = pymongo.collection.Collection(db, 'user_collection')
