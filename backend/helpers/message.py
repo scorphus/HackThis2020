df1ae0c182d4ed8cf9020031f89822c48f1ce339
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-08-09 12:06:39 -0700

    modified message

--- backend/helpers/message.py
+++ backend/helpers/message.py
@@ -8,22 +8,15 @@ socketio = SocketIO(app)
 
 @app.route('/<from_user>/messages/<to_user>')
 def sessions(from_user,to_user):
-    return render_template('message.html')
+    # maybe add authentication in the future
+    return render_template('message.html', to_user = to_user)
 
 def messageReceived(methods=['GET', 'POST']):
     print('Message Received')
 
-@socketio.on('join')
-def join_chat(data):
-    username = data['username']
-    room = data['room']
-    socketio.join_room(room)
-    socketio.send(username + ' has now joinned the chat.')
-
-@socketio.on('my event')
-def send_message_info(message_info, methods=['GET', 'POST']):
-    print('received my event: '+ str(message_info))
-    socketio.emit("my response", message_info, callback=messageReceived) #json info includes from_username, timestamp, message id, and message content.
+@socketio.on('message')
+def message(data):
+    socketio.send(data)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
