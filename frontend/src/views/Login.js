2836de212f30e7f0ba4663b5f9b68ffd02c15392
Noelle Crawford <noellegc3@gmail.com>
2020-08-13 09:29:23 -0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is behind 'origin/master' by 2 commits, and can be fast-forwarded.
       (use "git pull" to update your local branch)
    
     Changes to be committed:
    	modified:   src/views/Login.js
    
     Changes not staged for commit:
    	modified:   ../backend/app.py

--- frontend/src/views/Login.js
+++ frontend/src/views/Login.js
@@ -11,8 +11,8 @@ function Login() {
       console.log(data);
       const requestOptions = {
         method: 'POST',
+        headers: { 'Content-Type': 'application/json', 'SameSite':'None' },
         credentials: 'include',
-        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
       };
       fetch('http://127.0.0.1:5000/login', requestOptions).then(response => response.json()).then(data => console.log(data))
